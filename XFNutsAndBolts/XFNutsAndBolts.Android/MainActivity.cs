f73bbe13139d208ec41f75f168b8a46f8a9b1402
Riza Marhaban <riza.marhaban@hotmail.com>
2019-02-01 07:47:11 +0800

    Add project files.

---
+++ XFNutsAndBolts/XFNutsAndBolts.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Text;
+using XFNutsAndBolts.Droid.ConfigAndSettings;
+
+namespace XFNutsAndBolts.Droid
+{
+    [Activity(Label = "XFNutsAndBolts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // Can use injection if want it, but this loads only once
+            // no need to add resources just for loading one settings
+
+            var name1 = ConfigAndSetting.GalGagot;
+            var name2 = ConfigAndSetting.GalGadotInJapanese;
+            var json = Encoding.UTF8.GetString(ConfigAndSetting.appsettings);
+
+            LoadApplication(new App(name1, name2, json));
+        }
+    }
+}
---
+++ XFNutsAndBolts/XFNutsAndBolts/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace XFNutsAndBolts
+{
+    public partial class App : Application
+    {
+        public App(string name1, string name2, string json)
+        {
+            InitializeComponent();
+
+            // Can inject it or use directly here
+            Debug.WriteLine(name1);
+            Debug.WriteLine(name2);
+            Debug.WriteLine(json);
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
